c0f7e603ca8d31fce1664b4b8933d8090610224e
rilianx <rilianx@gmail.com>
2021-06-11 16:48:32 -0400

    new execute_bsg

--- base/box.py
+++ base/box.py
@@ -4,6 +4,11 @@ class box:
         self.x, self.y, self.z = dim
         self.vol = x*y*z
         self.complex = None
+        
+    def __init__(self, id, l, w, h, rotx, roty, rotz):
+        self.id = id
+        self.l = l; self.w = w; self.h = h;
+        self.rotx = rotx; self.roty = roty; self.rotz = rotz;
     
     def calculateComplex(self):
         expo = 1
